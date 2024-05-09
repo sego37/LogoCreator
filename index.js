836063b34d72c38fb62ef80c6a8e4d4b4395daee
Derek <sego37@gmail.com>
2024-05-09 16:38:53 -0600

    creating classes

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Logo = require('./lib/logo');
+
+inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'enter 3 characters?',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'enter text color?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'select shape?',
+        choices: ['circle','square', 'triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'enter shape color?',
+    }
+]).then(answers => {
+    console.log(answers);
+    const logo = new Logo(answers.text, answers.textColor, "circle", answers.shapeColor);
+
+    const logoSVG = logo.render();
+
+    console.log(logoSVG)
+
+    fs.writeFile('./examples/logo.svg', logoSVG, err => {
+        if (err) throw err;
+        console.log('file created');
+    });
+});
+
+
